368b66b8ab4cc27485b708ad5e0badc85cc66c57
natanbecker <natan.becker@code-ing.com>
2017-06-12 16:50:30 -0500

    First commit test quiz

---
+++ quiz/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Crea tus modelos de preguntas y respuestas para el quiz aquí.
+
+# class Question(models.Model):
+#
+#     class Meta:
+#         verbose_name = 'Pregunta'
+#         verbose_name_plural = 'Preguntas'
+#
+# class Respuesta(models.Model):
+#
+#     class Meta:
+#         verbose_name = 'Respuesta'
+#         verbose_name_plural = 'Respuestas'
+
